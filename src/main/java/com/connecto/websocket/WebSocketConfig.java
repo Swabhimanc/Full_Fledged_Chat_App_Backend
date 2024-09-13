a4555bb4f44e66cfc27e0fe88785a1adbc8d845f
swabhiman.chaurasiya <swabhiman.chaurasiya@dista.ai>
2024-09-13 10:31:52 +0530

    Added base URL and made changes to allowed origins

--- src/main/java/com/connecto/websocket/WebSocketConfig.java
+++ src/main/java/com/connecto/websocket/WebSocketConfig.java
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("http://localhost:3000","https://connecto-396305485425.asia-south1.run.app")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
