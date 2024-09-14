7c959637fc3ce7fa145b714aecf291b75e2a3a3f
swabhiman.chaurasiya <swabhiman.chaurasiya@dista.ai>
2024-09-14 13:02:29 +0530

    Trial Build

--- src/main/java/com/connecto/controller/IndexController.java
+++ src/main/java/com/connecto/controller/IndexController.java
@@ -3,14 +3,11 @@ package com.connecto.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
-public class IndexController implements ErrorController{
-    private final static String PATH = "/error";
-    @RequestMapping(PATH)
-    @ResponseBody
-    public String getErrorPath() {
-        // TODO Auto-generated method stub
-        return "No Mapping Found";
+public class IndexController    {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")  // Matches all routes that don't contain a period (.)
+    public String forward() {
+        return "forward:/index.html";  // Forward to React's index.html
     }
 }
