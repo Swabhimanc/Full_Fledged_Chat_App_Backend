5f9ad299952049fbf1d0247f110af6eb58a8515b
swabhiman.chaurasiya <swabhiman.chaurasiya@dista.ai>
2024-09-17 17:46:16 +0530

    Added fallbacks

--- src/main/java/com/connecto/controller/IndexController.java
+++ src/main/java/com/connecto/controller/IndexController.java
@@ -1,12 +1,11 @@
 package com.connecto.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController    {
 
-    @RequestMapping(value = "/{path:[^\\.]*}")  // Matches all routes that don't contain a period (.)
+    @GetMapping(value = { "/auth/**", "/app/**", "/{path:[^\\.]*}" }) // Matches all routes that don't contain a period (.)
     public String forward() {
         return "forward:/index.html";  // Forward to React's index.html
     }
